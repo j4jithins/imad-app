bc8a73c3fba9ea812341bc4481223f96a1239b91
Jithin Shaji <30323584+j4jithins@users.noreply.github.com>
2017-12-30 13:13:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    var articleOne: {
+
+    var articleOne = {
         title: 'Article One | Jithin Shaji',
         heading: 'Article One',
         date: 'December 29, 2017',
@@ -15,58 +15,58 @@ var articles = {
                 <p>This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.</p>
                 <p>This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.</p>
                 <p>This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.</p>`
-    },
-    var articleTwo : {
+    };
+    var articleTwo = {
         title: 'Article Two | Jithin Shaji',
         heading: 'Article Two',
         date: 'December 30, 2017',
         content: ` 
                 <p>This is the content of my Second article. This is the content of my Second article.</p>`
-    },
-    var articleThree :{
+    };
+    var articleThree ={
         title: 'Article Three | Jithin Shaji',
         heading: 'Article Three',
         date: 'December 31, 2017',
         content: ` 
                 <p>This is the content of my Third article.</p>`
-    }
-};
+    };
+
 function createTemplate(data){
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
     var content = data.content;
-var htmlTemplate=`<html>
-                    <head>
-                        <title>${title}</title>
-                        <style>
-                            .container{
-                                max-width: 800px;
-                                margin: 0 auto;
-                                color: gray;
-                                font-family: sans-serif;
-                            }
-                        </style>
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <link href="ui/style.css" rel="stylesheet"/>
-                    </head>
-                    <body>
-                        <div class="container">
-                            <div> <a href="/">Home</a> 
-                            </div>
-                            <hr>
-
-                            <h3>${heading}</h3>
-
-                            <div>${date}</div>
-
-                            <div> 
-                                ${content}
-
-                            </div>
-                        </div>    
-                    </body>
-                </html>
+        var htmlTemplate=`<html>
+                            <head>
+                                <title>${title}</title>
+                                <style>
+                                    .container{
+                                        max-width: 800px;
+                                        margin: 0 auto;
+                                        color: gray;
+                                        font-family: sans-serif;
+                                    }
+                                </style>
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <link href="ui/style.css" rel="stylesheet"/>
+                            </head>
+                            <body>
+                                <div class="container">
+                                    <div> <a href="/">Home</a> 
+                                    </div>
+                                    <hr>
+        
+                                    <h3>${heading}</h3>
+        
+                                    <div>${date}</div>
+        
+                                    <div> 
+                                        ${content}
+        
+                                    </div>
+                                </div>    
+                            </body>
+                        </html>
 `;
 return htmlTemplate;
 }
