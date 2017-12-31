77591c8bdeda5e38331f4cb72950e5224e8bd4d5
Jithin Shaji <30323584+j4jithins@users.noreply.github.com>
2017-12-31 10:21:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/;articleName', function(req, res){
+app.get('/:articleName', function(req, res){
     //articleName == articleOne
     //articles[articleName] == () content object for article-one
     res.send(createTemplate(articles[articleName]));
