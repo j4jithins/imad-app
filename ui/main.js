63d07ea84b8c0a0117667b416dee5c02c2df9fbf
Jithin Shaji <30323584+j4jithins@users.noreply.github.com>
2017-12-31 12:16:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
-var img = document.getElementById('madi');
-img.onClick = function() {
+var image = document.getElementById('madi');
+image.onClick = function() {
     console.log('Function called');
-    img.style.marginLeft = '100px';
+    image.style.marginLeft = '100px';
 };
