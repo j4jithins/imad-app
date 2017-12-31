a93df1197f3ee7b9244f99b0f602cebf71609f1c
Jithin Shaji <30323584+j4jithins@users.noreply.github.com>
2017-12-31 12:41:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,23 @@
 console.log('Loaded!');
 
+
 alert ('modified main.js');
 
+
 var element = document.getElementById('main-text');
-element.innerHTML = "new label";
+element.innerHTML = "New Text";
+
 
 var img = document.getElementById('madi');
+img.onclick = function() {
+img.style.marginLeft='100px';
+};
+//gradually move the image
+var marginLeft = 0;
+function moveRight() {
+marginLeft = marginLeft + 5;
+img.style.marginLeft= marginLeft + 'px';
+}
 img.onclick=function() {
-img.style.marginLeft+='100px';
+var interval = setInterval(moveRight,50);
 };
