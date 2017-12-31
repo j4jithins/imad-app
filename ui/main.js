12740ce2beaaeeb5432e30a64b45635ef24d505a
Jithin Shaji <30323584+j4jithins@users.noreply.github.com>
2017-12-31 14:59:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,5 @@ console.log('executed 0');
 var counter = 0;
 button.onClick = function (){
     console.log('executed 1');
-  //make request to counter end point
   
-  //Capture the response and store it in a variable
-  console.log('executed 2');
-  //Render the variable in the correct span
-  counter = counter + 1;
-  alert('counter');
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
 };
