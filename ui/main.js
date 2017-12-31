4d7259b83bcf2156e5d4e4af8d2f946929ebbcf4
Jithin Shaji <jithinshaji1298@gmail.com>
2017-12-31 12:10:33 +0530

    nothing special

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,6 @@ element.innerHTML = "New Value";
 
 var img = document.getElementById('madi');
 img.onClick = function() {
+    console.log('Function called');
     img.style.marginLeft = '100px';
 };
